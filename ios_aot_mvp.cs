c6e3ab6593f010f1adbd79083fbfb37b3c399ee9
Peter Mullen <peter.mullen@smokingguninc.com>
2024-05-29 16:47:07 +0100

    Initial commit

---
+++ ios_aot_mvp.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ios_aot_mvp;
+
+
+public static class ios_aot_mvp_api
+{
+    [UnmanagedCallersOnly( EntryPoint="sample_api_call" )]
+    public static int SampleApiCall()
+    {
+        return 42;
+    }
+}
